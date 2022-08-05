f45401869cce1c218a27f0221ea124e5c85bc7e3
Taras <93833612+TarasNetrebko@users.noreply.github.com>
2022-08-05 16:03:53 +0300

    easy

---
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Form, Label, Input, Button } from './PhonebookForm.styled';
+import PropTypes from "prop-types"
+
+export class PhonebookForm extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  }
+
+  state = {
+    name: '',
+    number: "",
+  };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+  addContact = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.setState({ name: '', number: "" });
+  };
+  render() {
+    const { name, number } = this.state;
+    return (
+      <Form action="" onSubmit={this.addContact}>
+        <Label>
+          Name
+          <Input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={this.handleChange}
+          />
+        </Label>
+        <Label>
+            Number
+            <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={this.handleChange}
+          />
+          </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    );
+  }
+}
